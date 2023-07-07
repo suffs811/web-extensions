ef7d745b85503e62c1d145a444be8087872b70f5
suffs811 <84633135+suffs811@users.noreply.github.com>
2023-07-07 17:04:08 -0400

    Update content.js

--- library-search/content.js
+++ library-search/content.js
@@ -1,13 +1,18 @@
-// get search text from textarea and search url
+// get text and search library
 function search() {
 	var selection = textArea.value;
 	var url = "https://scholar.google.com/scholar?hl=en&as_sdt=0%2C11&&btnG=&q=" + selection
 	if(selection.length > 0) {
-	window.open(url)
+		window.open(url);
+		window.close();
 	}
-}
+};
 
-// get elements and listen for button click
+// get textarea and upon pressing 'enter' key, exec search()
 var textArea = document.getElementById("text");
+textArea.focus();
+textArea.addEventListener("keypress", function(event){if(event.key === "Enter"){search()}});
+
+// get button and upon button press, exec search()
 var button = document.getElementById("button");
-button.addEventListener("click", (e) => search())
+button.addEventListener("click", (e) => search());
